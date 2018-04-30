8963a04247fb9429ef7f530e8d9694e5b240d628
Herman Bergner <hermanbergner@gmail.com>
2018-04-30 21:19:03 +0200

    temp

---
+++ canvas/src/server/index.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+
+const HOST = process.env.HOST || '0.0.0.0'
+const PORT = process.env.PORT || 8000
+
+
+
+app.use(express.static(path.resolve(__dirname, '../../dist')))
+
+app.listen(PORT, HOST, () => {
+    console.log(`server is running on ${HOST}:${PORT}`)
+})
